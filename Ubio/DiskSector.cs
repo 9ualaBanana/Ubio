c2a2464ddf0703bb56b778a8c09786b1f858344e
GIBSON <9ualabanana@gmail.com>
2022-09-13 17:49:29 +0300

    Add DiskSector.

---
+++ Ubio/DiskSector.cs
@@ -0,0 +1,25 @@
+﻿namespace System.IO;
+
+public class DiskSector
+{
+    public readonly int LogicalSize;
+    public readonly int PhysicalSize;
+    public bool IsAligned(long fileAccessSize) => fileAccessSize % LogicalSize == 0;
+
+
+    internal DiskSector(string path)
+    {
+        (LogicalSize, PhysicalSize) = Win32.GetSectorSizes(Path.IsPathRooted(path) ? Path.GetPathRoot(path) : null);
+    }
+
+
+    public int EnsureIsAligned(int fileAccessSize) => (int)EnsureIsAligned((long)fileAccessSize);
+    public long EnsureIsAligned(long fileAccessSize)
+    {
+        if (!IsAligned(fileAccessSize))
+            throw new ArgumentOutOfRangeException(
+                nameof(fileAccessSize),
+                "Unbuffered file access sizes must be for a number of bytes that is an integer multiple of the volume sector size.");
+        return fileAccessSize;
+    }
+}
