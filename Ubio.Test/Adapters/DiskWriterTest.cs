36d0290e4df840e9e988cef39e6893a4c27fa9d3
GIBSON <9ualabanana@gmail.com>
2022-09-16 14:37:23 +0300

    Add disk writer tests.

---
+++ Ubio.Test/Adapters/DiskWriterTest.cs
@@ -0,0 +1,71 @@
+﻿namespace Ubio.Test.Adapters;
+
+public class DiskWriterTest
+{
+    [Fact]
+    public void WriteSector_WritesLogicalSector()
+    {
+        var unbufferedFileStream = TestData.TestFile;
+        using var diskSectorWriter = new DiskSectorWriter(unbufferedFileStream);
+
+        diskSectorWriter.WriteSector(new byte[unbufferedFileStream.DiskSector.LogicalSize]);
+
+        unbufferedFileStream.Length.Should().Be(unbufferedFileStream.DiskSector.LogicalSize);
+    }
+    
+    [Fact]
+    public void WriteSector_WritesPhysicalSector()
+    {
+        var unbufferedFileStream = TestData.TestFile;
+        using var diskSectorWriter = new DiskSectorWriter(unbufferedFileStream);
+
+        diskSectorWriter.WriteSector(new byte[unbufferedFileStream.DiskSector.PhysicalSize]);
+
+        unbufferedFileStream.Length.Should().Be(unbufferedFileStream.DiskSector.PhysicalSize);
+    }
+
+    [Fact]
+    public void WriteLogicalSector_LogicalSectorSizeNumberOfBytes_WritesThatNumberOfBytes()
+    {
+        var unbufferedFileStream = TestData.TestFile;
+        using var diskSectorWriter = new DiskSectorWriter(unbufferedFileStream);
+
+        diskSectorWriter.WriteLogicalSector(new byte[unbufferedFileStream.DiskSector.LogicalSize]);
+
+        unbufferedFileStream.Length.Should().Be(unbufferedFileStream.DiskSector.LogicalSize);
+    }
+    
+    [Fact]
+    public void WriteLogicalSector_PhysicalSectorSizeNumberOfBytes_WritesLogicalSectorSizeNumberOfBytes()
+    {
+        var unbufferedFileStream = TestData.TestFile;
+        using var diskSectorWriter = new DiskSectorWriter(unbufferedFileStream);
+
+        diskSectorWriter.WriteLogicalSector(new byte[unbufferedFileStream.DiskSector.PhysicalSize]);
+
+        unbufferedFileStream.Length.Should().Be(unbufferedFileStream.DiskSector.LogicalSize);
+    }
+    
+    [Fact]
+    public void WritePhysicalSector_PhysicalSectorSizeNumberOfBytes_WritesThatNumberOfBytes()
+    {
+        var unbufferedFileStream = TestData.TestFile;
+        using var diskSectorWriter = new DiskSectorWriter(unbufferedFileStream);
+
+        diskSectorWriter.WritePhysicalSector(new byte[unbufferedFileStream.DiskSector.PhysicalSize]);
+
+        unbufferedFileStream.Length.Should().Be(unbufferedFileStream.DiskSector.PhysicalSize);
+    }
+    
+    [Fact]
+    public void WritePhysicalSector_LogicalSectorSizeNumberOfBytes_Throws()
+    {
+        var unbufferedFileStream = TestData.TestFile;
+        using var diskSectorWriter = new DiskSectorWriter(unbufferedFileStream);
+
+        Action writing = () => diskSectorWriter.WritePhysicalSector(new byte[unbufferedFileStream.DiskSector.LogicalSize]);
+
+        writing.Should().Throw<ArgumentOutOfRangeException>();
+    }
+
+}
---
+++ Ubio.Test/TestData.cs
@@ -0,0 +1,8 @@
+﻿namespace Ubio.Test;
+
+internal static class TestData
+{
+    internal static UnbufferedFileStream TestFile => new UnbufferedFileStream(
+        Path.GetRandomFileName(),
+        new FileStreamOptions { Mode = FileMode.Create, Access = FileAccess.ReadWrite, Options = FileOptions.DeleteOnClose });
+}
