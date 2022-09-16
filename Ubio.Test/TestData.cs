a1095f892960295f1889bdf09252362c259d3fad
GIBSON <9ualabanana@gmail.com>
2022-09-16 15:20:14 +0300

    Add tests for static input wrappers.

--- Ubio.Test/TestData.cs
+++ Ubio.Test/TestData.cs
@@ -5,4 +5,17 @@ internal static class TestData
     internal static UnbufferedFileStream TestFile => new UnbufferedFileStream(
         Path.GetRandomFileName(),
         new FileStreamOptions { Mode = FileMode.Create, Access = FileAccess.ReadWrite, Options = FileOptions.DeleteOnClose });
+
+    public static IEnumerable<object[]> NonSectorAlignedNumberOfBytes
+    {
+        get
+        {
+            int sectorAlignedNumberOfBytes;
+            using (var unbufferedFileStream = UnbufferedFile.OpenRead(Path.GetTempFileName()))
+                sectorAlignedNumberOfBytes = unbufferedFileStream.DiskSector.PhysicalSize;
+
+            yield return new object[] { (sectorAlignedNumberOfBytes - 1, sectorAlignedNumberOfBytes) };
+            yield return new object[] { (sectorAlignedNumberOfBytes + 1, 2 * sectorAlignedNumberOfBytes) };
+        }
+    }
 }
---
+++ Ubio.Test/UnbufferedFileTest.cs
@@ -0,0 +1,30 @@
+﻿namespace Ubio.Test;
+
+public class UnbufferedFileTest
+{
+    [Fact]
+    public void WriteAllBytes_SectorAlignedNumberOfBytes_WritesThatNumberOfBytes()
+    {
+        var path = Path.GetTempFileName();
+        int sectorAlignedNumberOfBytes;
+        using (var unbufferedFileStream = UnbufferedFile.OpenRead(path))
+            sectorAlignedNumberOfBytes = unbufferedFileStream.DiskSector.PhysicalSize;
+
+        UnbufferedFile.WriteAllBytes(path, new byte[sectorAlignedNumberOfBytes]);
+
+        UnbufferedFile.OpenRead(path).Length.Should().Be(sectorAlignedNumberOfBytes);
+    }
+    
+    [Theory]
+    [MemberData(nameof(TestData.NonSectorAlignedNumberOfBytes), MemberType = typeof(TestData))]
+    public void WriteAllBytes_NonSectorAlignedNumberOfBytes_WritesExtraSectorAlignedNumberOfBytes(
+        (int NonSectorAligned, int SectorAligned) numberOfBytes
+        )
+    {
+        var path = Path.GetTempFileName();
+
+        UnbufferedFile.WriteAllBytes(path, new byte[numberOfBytes.NonSectorAligned]);
+
+        UnbufferedFile.OpenRead(path).Length.Should().Be(numberOfBytes.SectorAligned);
+    }
+}
